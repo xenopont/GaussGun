e6d6cd03236ddd6766dc7b999be307425be81245
smith404@live.com <smith404@live.com>
2023-02-13 20:41:01 +0100

    window rectagnle plus z-order, win32api refactored

--- GaussGun/Experiment.cs
+++ GaussGun/Experiment.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GaussGun
 {
@@ -13,62 +10,51 @@ namespace GaussGun
             public readonly IntPtr hWnd;
             public readonly string title;
             public readonly bool isIconic;
-            public readonly Win32.Rect rect;
+            public readonly Win32Api.Rectangle rectangle;
+            public readonly uint zOrder;
 
-            public Win32Window(IntPtr hWnd, string title, bool isIconic, Win32.Rect rect)
+            public Win32Window(
+                IntPtr hWnd,
+                string title,
+                bool isIconic,
+                Win32Api.Rectangle rectangle,
+                uint zOrder
+            )
             {
                 this.hWnd = hWnd;
                 this.title = title;
                 this.isIconic = isIconic;
-                this.rect = rect;
+                this.rectangle = rectangle;
+                this.zOrder = zOrder;
             }
         }
 
         public static List<Win32Window> ListAllWindows()
         {
             List<Win32Window> windows = new();
+            uint zOrder = 0;
 
-            _ = Win32.EnumWindows((IntPtr hWnd, int lParam) =>
+            Win32Api.EnumerateWindows((IntPtr hWnd) =>
             {
-                bool isCloaked = IsWindowCloaked(hWnd);
-                bool isIconic = Win32.IsIconic(hWnd);
-                string title = GetWindowTitle(hWnd);
-                Win32.GetWindowRect(hWnd, out var rect);
-                if (IsWindowVisible(hWnd) && !isCloaked)
+                bool isCloaked = Win32Api.IsWindowCloaked(hWnd);
+                bool isVisible = Win32Api.IsWindowVisible(hWnd);
+                bool isIconic = Win32Api.IsWindowIconic(hWnd);
+                string title = Win32Api.GetWindowTitle(hWnd);
+                Win32Api.Rectangle rectangle = Win32Api.GetWindowRectangle(hWnd);
+                if (isVisible && !isCloaked)
                 {
-                    windows.Add(new Win32Window(hWnd, $"{title} {rect}", isIconic, rect));
+                    windows.Add(new Win32Window(
+                        hWnd,
+                        $"{zOrder} - {title} {rectangle}",
+                        isIconic,
+                        rectangle,
+                        zOrder++
+                    ));
                 }
                 return true;
-            }, 0);
+            });
 
             return windows;
         }
-
-        private static bool IsWindowVisible(IntPtr hWnd)
-        {
-            return Win32.IsWindowVisible(hWnd) > 0;
-        }
-
-        private static bool IsWindowCloaked(IntPtr hWnd)
-        {
-            int isCloaked = 0;
-            uint ret = Win32.DwmGetWindowAttribute(hWnd, Win32.DwmWindowAttribute.DWMWA_CLOAKED, ref isCloaked, 4);
-            return ret == 0 && isCloaked > 0;
-        }
-
-        private static string GetWindowTitle(IntPtr hWnd)
-        {
-            int length = Win32.GetWindowTextLength(hWnd);
-            if (length == 0)
-            {
-                return NO_TITLE;
-            }
-            StringBuilder sb = new(length + 1);
-            int res = Win32.GetWindowText(hWnd, sb, sb.Capacity);
-            if (res == 0) { return "ERROR GETTING TITLE"; }
-            return sb.ToString();
-        }
-
-        private const string NO_TITLE = "NO TITLE";
     }
 }
