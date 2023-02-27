e460898ca2345d93e33922027bc837d8cab3dffb
Sergei Kovalenko <smith404@live.com>
2023-02-27 20:58:03 +0100

    Enum windows (#1)
    * list hwnd
    
    * window titles
    
    * cloaked fixed
    
    * is iconic thing
    
    * rect structure, better window data
    
    * window rectagnle plus z-order, win32api refactored
    
    * some other invisible windows removed

---
+++ GaussGun/Experiment.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GaussGun
+{
+    internal class Experiment {
+        public class Win32Window
+        {
+            public readonly IntPtr hWnd;
+            public readonly string title;
+            public readonly bool isIconic;
+            public readonly Win32Api.Rectangle rectangle;
+            public readonly uint zOrder;
+
+            public Win32Window(
+                IntPtr hWnd,
+                string title,
+                bool isIconic,
+                Win32Api.Rectangle rectangle,
+                uint zOrder
+            )
+            {
+                this.hWnd = hWnd;
+                this.title = title;
+                this.isIconic = isIconic;
+                this.rectangle = rectangle;
+                this.zOrder = zOrder;
+            }
+        }
+
+        public static List<Win32Window> ListAllWindows()
+        {
+            List<Win32Window> windows = new();
+            uint zOrder = 0;
+
+            Win32Api.EnumerateWindows((IntPtr hWnd) =>
+            {
+                bool isCloaked = Win32Api.IsWindowCloaked(hWnd);
+                bool isVisible = Win32Api.IsWindowVisible(hWnd);
+                bool isIconic = Win32Api.IsWindowIconic(hWnd);
+                string isDesktopWindowSign = hWnd == hDesktopWindow ? "*" : "";
+                string title = Win32Api.GetWindowTitle(hWnd);
+                Win32Api.Rectangle rectangle = Win32Api.GetWindowRectangle(hWnd);
+                if (isVisible && !isCloaked && IsOnScreen(rectangle))
+                {
+                    windows.Add(new Win32Window(
+                        hWnd,
+                        $"{isDesktopWindowSign}{zOrder} - {title} {rectangle}",
+                        isIconic,
+                        rectangle,
+                        zOrder++
+                    ));
+                }
+                return true;
+            });
+
+            return windows;
+        }
+
+        private static bool IsOnScreen(Win32Api.Rectangle rectangle) {
+            return rectangle.Height > 0 && rectangle.Width > 0 &&
+                // left top corner not farther than bottom right of the screen
+                rectangle.Left < DesktopWindowRectangle.Left + DesktopWindowRectangle.Width &&
+                rectangle.Top < DesktopWindowRectangle.Top + DesktopWindowRectangle.Height &&
+                // bottom right corner is not in "negative" area
+                rectangle.Left + rectangle.Width > DesktopWindowRectangle.Left &&
+                rectangle.Top + rectangle.Height > DesktopWindowRectangle.Top &&
+                // system invisible windows like "Program Manager"
+                rectangle != DesktopWindowRectangle;
+        }
+
+        private static readonly IntPtr hDesktopWindow = Win32Api.GetDesktopWindow();
+        private static readonly Win32Api.Rectangle DesktopWindowRectangle =
+            Win32Api.GetWindowRectangle(hDesktopWindow);
+    }
+}
---
+++ GaussGun/Win32Api.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.CodeDom;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace GaussGun
+{
+    internal class Win32Api
+    {
+        public delegate bool EnumerateWindowsCallback(IntPtr hWnd);
+        public static void EnumerateWindows(EnumerateWindowsCallback callback)
+        {
+            _ = RawApi.EnumWindows((IntPtr hWnd, int lParam) =>
+            {
+                return callback(hWnd);
+            }, 0);
+        }
+
+        public static IntPtr GetDesktopWindow() {
+            return RawApi.GetDesktopWindow();
+        }
+
+        public static Rectangle GetWindowRectangle(IntPtr hWnd)
+        {
+            int hResult = RawApi.GetWindowRect(hWnd, out RawApi.RECT rectangle);
+            if (hResult == 0)
+            {
+                return new Rectangle();
+            }
+
+            return new Rectangle(
+                rectangle.left,
+                rectangle.top,
+                rectangle.right - rectangle.left,
+                rectangle.bottom - rectangle.top
+            );
+        }
+
+        #region Remove after window title is not needed
+        public static string GetWindowTitle(IntPtr hWnd)
+        {
+            int length = RawApi.GetWindowTextLength(hWnd);
+            if (length <= 0)
+            {
+                return "NO TITLE";
+            }
+            StringBuilder sb = new(length + 1);
+            int res = RawApi.GetWindowText(hWnd, sb, sb.Capacity);
+            if (res == 0)
+            {
+                return "ERROR GETTING TITLE";
+            }
+            return sb.ToString();
+        }
+        #endregion
+
+        public static bool IsWindowCloaked(IntPtr hWnd)
+        {
+            int isCloaked = 0;
+            uint hResult = RawApi.DwmGetWindowAttribute(
+                hWnd,
+                RawApi.DwmWindowAttribute.DWMWA_CLOAKED,
+                ref isCloaked,
+                4
+            );
+
+            return hResult == 0 && isCloaked > 0;
+        }
+
+        public static bool IsWindowIconic(IntPtr hWnd)
+        {
+            return RawApi.IsIconic(hWnd) > 0;
+        }
+
+        public static bool IsWindowVisible(IntPtr hWnd)
+        {
+            return RawApi.IsWindowVisible(hWnd) > 0;
+        }
+
+        public readonly struct Rectangle
+        {
+            public readonly int Left;
+            public readonly int Top;
+            public readonly int Width;
+            public readonly int Height;
+
+            public Rectangle(int left, int top, int width, int height)
+            {
+                Left = left;
+                Top = top;
+                Width = width;
+                Height = height;
+            }
+
+            public Rectangle() : this(0, 0, 0, 0) { }
+
+            public override string ToString()
+            {
+                return $"[{Left}, {Top}; {Width}, {Height}]";
+            }
+
+            public static bool operator ==(Rectangle r1, Rectangle r2) {
+                return r1.Left == r2.Left && r1.Top == r2.Top &&
+                    r1.Width == r2.Width && r1.Height == r2.Height;
+            }
+
+            public static bool operator !=(Rectangle r1, Rectangle r2) {
+                return r1.Left != r2.Left || r1.Top != r2.Top ||
+                    r1.Width != r2.Width || r1.Height != r2.Height;
+            }
+        }
+
+        private static class RawApi
+        {
+            private const string DLL_DWM_API = "dwmapi.dll";
+            private const string DLL_USER_32 = "user32.dll";
+
+            [DllImport(DLL_DWM_API)]
+            public static extern uint DwmGetWindowAttribute(
+                IntPtr hWnd,
+                DwmWindowAttribute attributeName,
+                ref int attributeValue,
+                int cbSize
+            );
+            public enum DwmWindowAttribute : uint
+            {
+                DWMWA_NCRENDERING_ENABLED = 1,
+                DWMWA_NCRENDERING_POLICY,
+                DWMWA_TRANSITIONS_FORCEDISABLED,
+                DWMWA_ALLOW_NCPAINT,
+                DWMWA_CAPTION_BUTTON_BOUNDS,
+                DWMWA_NONCLIENT_RTL_LAYOUT,
+                DWMWA_FORCE_ICONIC_REPRESENTATION,
+                DWMWA_FLIP3D_POLICY,
+                DWMWA_EXTENDED_FRAME_BOUNDS,
+                DWMWA_HAS_ICONIC_BITMAP,
+                DWMWA_DISALLOW_PEEK,
+                DWMWA_EXCLUDED_FROM_PEEK,
+                DWMWA_CLOAK,
+                DWMWA_CLOAKED,
+                DWMWA_FREEZE_REPRESENTATION,
+                DWMWA_LAST
+            }
+
+            public delegate bool WNDENUMPROC(IntPtr hWnd, int lParam);
+            [DllImport(DLL_USER_32)]
+            public static extern int EnumWindows(WNDENUMPROC EnumWindowsProc, int lParam);
+
+            [DllImport(DLL_USER_32)]
+            public static extern IntPtr GetDesktopWindow();
+
+            [DllImport(DLL_USER_32)]
+            public static extern int GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+            #region To remove after window title not needed
+            [DllImport(DLL_USER_32, CharSet = CharSet.Unicode, SetLastError = true)]
+            public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+            [DllImport(DLL_USER_32)]
+            public static extern int GetWindowTextLength(IntPtr hWnd);
+            #endregion
+
+            [DllImport(DLL_USER_32)]
+            public static extern int IsIconic(IntPtr hWnd);
+
+            [DllImport(DLL_USER_32)]
+            public static extern int IsWindowVisible(IntPtr hWnd);
+
+            public struct RECT
+            {
+                public int left;
+                public int top;
+                public int right;
+                public int bottom;
+            }
+        }
+    }
+}
