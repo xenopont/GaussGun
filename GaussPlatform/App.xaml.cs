b85672cc47f4c4156bdc23441400aa3fb43b3ae2
Sergei Kovalenko <smith404@live.com>
2025-10-29 19:30:15 +0100

    Add/Remove tray icon

--- GaussPlatform/App.xaml.cs
+++ GaussPlatform/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace GaussPlatform;
 
@@ -8,4 +9,39 @@ namespace GaussPlatform;
 /// </summary>
 public partial class App
 {
+    private NotifyIcon? _notifyIcon;
+        
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var stream = (GetResourceStream(new Uri("pack://application:,,,/Resources/Icons/TrayIcon/TrayIcon.ico")));
+        if (stream == null)
+        {
+            return;
+        }
+        _notifyIcon = new NotifyIcon
+        {
+            Icon = new Icon(stream.Stream),
+            Visible = true,
+            Text = "My WPF Tray App"
+        };
+
+        var contextMenu = new ContextMenuStrip();
+        contextMenu.Items.Add("Show Settings", null, null);
+        contextMenu.Items.Add("Exit", null, OnExitClicked);
+        _notifyIcon.ContextMenuStrip = contextMenu;
+    }
+    
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _notifyIcon?.Visible = false;
+        _notifyIcon?.Dispose();
+        base.OnExit(e);
+    }
+    
+    private void OnExitClicked(object? sender, EventArgs e)
+    {
+        Shutdown();
+    }
 }
